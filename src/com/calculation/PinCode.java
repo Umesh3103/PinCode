13f228a3767b6ae34ed54b66ea673620f2506622
DESKTOP-1260D35\Hp <deoraumesh32@gmail.com>
2020-09-28 15:31:08 +0530

    [Umesh]Added: checking for space between 3 digit

--- src/com/calculation/PinCode.java
+++ src/com/calculation/PinCode.java
@@ -2,14 +2,14 @@ package com.calculation;
 import java.util.regex.*;
 
 public class PinCode {
-	private static final Pattern NAME_PATTERN = Pattern.compile("^[0-9]{6}$");
+	private static final Pattern NAME_PATTERN = Pattern.compile("^[0-9]{3}\\s{0,1}[0-9]{3}$");
     private static boolean checkName(String pin) {
         return NAME_PATTERN.matcher(pin).matches();
     }
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to pin code pattern problem");
-		boolean flag = checkName("400088");
+		boolean flag = checkName("400 088");
 		System.out.println(flag);
 	}
 }
