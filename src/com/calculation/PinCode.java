b638a67528e3145f923962723bd043836a6cd040
DESKTOP-1260D35\Hp <deoraumesh32@gmail.com>
2020-09-28 15:00:24 +0530

    [Umesh]Added: welcome message

---
+++ src/com/calculation/PinCode.java
@@ -0,0 +1,8 @@
+package com.calculation;
+import java.util.regex.*;
+
+public class PinCode {
+	public static void main(String[] args) {
+		System.out.println("Welcome to pin code pattern problem");
+	}
+}
