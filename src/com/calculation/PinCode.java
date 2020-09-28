855c9364a4d760c0ca5fab85bc05e37add804495
DESKTOP-1260D35\Hp <deoraumesh32@gmail.com>
2020-09-28 15:05:25 +0530

    [Umesh3103]Added: checking pattern for pin code

--- src/com/calculation/PinCode.java
+++ src/com/calculation/PinCode.java
@@ -2,7 +2,14 @@ package com.calculation;
 import java.util.regex.*;
 
 public class PinCode {
+	private static final Pattern NAME_PATTERN = Pattern.compile("[0-9]{6}");
+    private static boolean checkName(String pin) {
+        return NAME_PATTERN.matcher(pin).matches();
+    }
+
 	public static void main(String[] args) {
 		System.out.println("Welcome to pin code pattern problem");
+		boolean flag = checkName("400088");
+		System.out.println(flag);
 	}
 }
